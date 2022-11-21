b1ba05b4afa7ed62d82c11310c62de430fa1b2a0
Nate Harris <nathanharris.sf@gmail.com>
2022-11-21 09:14:13 -0800

    db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,18 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\ProductQuantity;
+use App\Models\ProductVariant;
+use App\Models\Variant;
 use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Testing\WithFaker;
 
 class DatabaseSeeder extends Seeder
 {
+    use WithFaker;
+
     /**
      * Seed the application's database.
      *
@@ -14,11 +21,71 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->setUpFaker();
+        $this->seedCategories();
+        $this->seedVariants();
+        $this->seedProducts();
+    }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    private function seedCategories(): void
+    {
+        collect(['shirts', 'pants', 'socks'])->each(function (string $category) {
+            Category::firstOrCreate(['name' => $category]);
+        });
+    }
+
+    private function seedVariants(): void
+    {
+        collect(['small', 'medium', 'large'])->each(function (string $variant) {
+            Variant::firstOrCreate(['name' => $variant]);
+        });
+    }
+
+    private function seedProducts(): void
+    {
+        $variants = Variant::all();
+        $productTypes = [
+            'shirts' => ['Fleece', 'T-Shirt', 'Sweater', 'Button Up'],
+            'pants' => ['Shorts', 'Dress Pants', 'Jeans', 'Pajamas'],
+            'socks' => ['No Show Socks', 'Ankle Socks', 'Running Socks']
+        ];
+
+        foreach (Category::all() as $category) {
+            foreach ($productTypes[$category->name] as $type) {
+                foreach (range(0, rand(5, 15)) as $unused) {
+                    // generate the product name
+                    $name = collect([
+                        $this->faker->city(),
+                        $this->faker->firstName(),
+                        $this->faker->lastName(),
+                    ])->random();
+                    $productName = $name . ' ' . $type;
+
+                    // create the product
+                    $product = Product::firstOrCreate([
+                        'name' => $productName,
+                        'category_id' => $category->id,
+                    ]);
+
+                    // create the product variants
+                    $productVariants = collect();
+                    foreach ($variants as $variant) {
+                        $productVariants->push(ProductVariant::firstOrCreate([
+                            'product_id' => $product->id,
+                            'variant_id' => $variant->id,
+                            'price' => rand(10, 400),
+                        ]));
+                    }
+
+                    // create the stock quantity for each variant
+                    foreach ($productVariants as $productVariant) {
+                        ProductQuantity::firstOrCreate([
+                            'product_variant_id' => $productVariant->id,
+                            'amount' => rand(2, 30),
+                        ]);
+                    }
+                }
+            }
+        }
     }
 }
